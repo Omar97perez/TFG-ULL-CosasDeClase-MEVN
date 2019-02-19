07f47904bc2fdebaae3a177edd031b4141df56aa
Omar97perez <omarperezznakar@gmail.com>
2019-02-19 20:15:38 +0000

    Prototipo: Configuración básica 2

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Posts from '@/components/Posts'
 import Index from '@/components/index'
+import Clases from '@/components/clases_particulares'
+import Apuntes from '@/components/apuntes'
+import Noticias from '@/components/noticias'
+import Contacto from '@/components/contacto'
+import Perfil from '@/components/perfil'
 
 Vue.use(Router)
 
@@ -12,6 +17,31 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/ClasesParticulares',
+      name: 'clases',
+      component: Clases
+    },
+    {
+      path: '/Apuntes',
+      name: 'apuntes',
+      component: Apuntes
+    },
+    {
+      path: '/Noticias',
+      name: 'noticias',
+      component: Noticias
+    },
+    {
+      path: '/Contacto',
+      name: 'contacto',
+      component: Contacto
+    },
+    {
+      path: '/Perfil',
+      name: 'perfil',
+      component: Perfil
     }
   ]
 })
