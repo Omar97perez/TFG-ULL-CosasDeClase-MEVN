71dd41ef8822288e230d43bea74575daf6b40b05
Omr97perez <omarperezznakar@gmail.com>
2019-03-20 20:08:50 +0000

    Frontend: Creando ficheros buscador

---
+++ app/src/app/components/productos/buscador_noticias.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <!--/ Intro Single star /-->
+    <section class="intro-single">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-lg-8">
+            <div class="title-single-box">
+              <h1 class="title-single">{{prev.titulo}}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--/ Intro Single End /-->
+
+    <!--/ News Single Star /-->
+    <section class="news-single nav-arrow-b">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="news-img-box">
+                  <img class="img-fluid" :src="prev.foto">
+            </div>
+          </div>
+          <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+            <div class="post-information">
+              <ul class="list-inline text-center color-a">
+                <li class="list-inline-item mr-2">
+                  <strong>Author: </strong>
+                  <span class="color-text-a">{{prev.anunciante}}</span>
+                </li>
+                <!-- <li class="list-inline-item mr-2">
+                  <strong>Category: </strong>
+                  <span class="color-text-a">Travel</span>
+                </li> -->
+                <li class="list-inline-item">
+                  <strong>Date: </strong>
+                  <span class="color-text-a">{{prev.fecha}}</span>
+                </li>
+              </ul>
+            </div>
+            <div class="post-content color-text-a">
+              <p>{{prev.descripcion}}</p>
+            </div>
+            <div class="post-footer">
+              <div class="post-share">
+                <span>Share: </span>
+                <ul class="list-inline socials">
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--/ News Single End /-->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'anuncio_noticia',
+  data(){
+      return{
+          Productos: [],
+      }
+  },
+  created() {
+      this.getProductos();
+  },
+  methods: {
+    getProductos() {
+      fetch('/api/CosasDeClase/Producto/')
+        .then(res => res.json())
+        .then(data => {
+          this.Productos = data;
+        });
+    },
+  },
+  computed: {
+    prev(){
+        return this.Productos.find((PrevItem) =>{
+            return PrevItem._id === this.$store.getters.preview;
+        });
+    },
+  },
+};
+</script>
---
+++ app/src/app/components/productos/buscador_productos.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+
+    <!--/ Intro Single star /-->
+    <section class="intro-single">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-lg-8">
+            <div class="title-single-box">
+              <h1 class="title-single">{{prev.titulo}}</h1>
+              <span class="color-text-a">{{prev.localidad}} ({{prev.provincia}})</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--/ Intro Single End /-->
+
+    <!--/ Property Single Star /-->
+    <section class="property-single nav-arrow-b">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="row justify-content-between">
+              <div class="col-md-5 col-lg-4">
+                <div class="property-price d-flex justify-content-center foo">
+                      <img class="prod-img" :src="prev.foto">
+                </div>
+                </br>
+                <div class="property-price d-flex justify-content-center foo">
+                  <div class="card-header-c d-flex">
+                    <div class="card-box-ico">
+                      <h1 class="ion-money">{{prev.precio}} €</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-7 col-lg-7 section-md-t3">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <div class="title-box-d">
+                      <h3 class="title-d">Descripción</h3>
+                    </div>
+                  </div>
+                </div>
+                <div class="property-description">
+                  <p class="description color-text-a">{{prev.descripcion}}</p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="row section-t3">
+              <div class="col-sm-12">
+                <div class="title-box-d">
+                  <h3 class="title-d">Anunciante</h3>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6 col-lg-4">
+                <img src="img/agent-4.jpg" alt="" class="img-fluid">
+              </div>
+              <div class="col-md-6 col-lg-7">
+                <div class="property-agent">
+                  <h4 class="title-agent">Anabella Geller</h4>
+                  <p class="color-text-a">
+                    Nulla porttitor accumsan tincidunt. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
+                    dui. Quisque velit nisi,
+                    pretium ut lacinia in, elementum id enim.
+                  </p>
+                  <ul class="list-unstyled">
+                    <li class="d-flex justify-content-between">
+                      <strong>Phone:</strong>
+                      <span class="color-text-a">(222) 4568932</span>
+                    </li>
+                    <li class="d-flex justify-content-between">
+                      <strong>Mobile:</strong>
+                      <span class="color-text-a">777 287 378 737</span>
+                    </li>
+                    <li class="d-flex justify-content-between">
+                      <strong>Email:</strong>
+                      <span class="color-text-a">annabella@example.com</span>
+                    </li>
+                    <li class="d-flex justify-content-between">
+                      <strong>Skype:</strong>
+                      <span class="color-text-a">Annabela.ge</span>
+                    </li>
+                  </ul>
+                  <div class="socials-a">
+                    <ul class="list-inline">
+                      <li class="list-inline-item">
+                        <a href="#">
+                          <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                      <li class="list-inline-item">
+                        <a href="#">
+                          <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                      <li class="list-inline-item">
+                        <a href="#">
+                          <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                      <li class="list-inline-item">
+                        <a href="#">
+                          <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                      <li class="list-inline-item">
+                        <a href="#">
+                          <i class="fa fa-dribbble" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'anuncio',
+  data(){
+      return{
+          Productos: [],
+      }
+  },
+  created() {
+      this.getProductos();
+  },
+  methods: {
+    getProductos() {
+      fetch('/api/CosasDeClase/Producto/')
+        .then(res => res.json())
+        .then(data => {
+          this.Productos = data;
+        });
+    },
+  },
+  computed: {
+    prev(){
+        return this.Productos.find((PrevItem) =>{
+            return PrevItem._id === this.$store.getters.preview;
+        });
+    },
+  },
+};
+</script>
